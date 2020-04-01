5afc46d40d8aa39b7d5b89cc9904cc8ee38e8421
menglingyu <897205285@qq.com>
2020-04-01 19:09:15 +0800

    react

---
+++ precache-manifest.bb8688a3c540d96e6687609a6a401dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23db6b52d9d0d07fb486d53a3876fc9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84ef9c3d04d3741a295d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8406b57d7bb6bed3c3e5",
+    "url": "/static/js/2.4f355300.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4f355300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ef9c3d04d3741a295d",
+    "url": "/static/js/main.1ae6d7a1.chunk.js"
+  },
+  {
+    "revision": "aadb658bf0a6b0d1c2ad",
+    "url": "/static/js/runtime-main.85d6ab96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
