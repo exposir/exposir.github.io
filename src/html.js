0dec6f08f5e8e708e25c88851ea2e5918f40b327
menglingyu <exposir@foxmail.com>
2021-07-21 17:24:06 +0800

    更新博客

--- src/html.js
+++ src/html.js
@@ -4,15 +4,6 @@ import PropTypes from "prop-types";
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
-      <script>
-        {`var _hmt = _hmt || [];
-(function() {
-var hm = document.createElement("script");
-hm.src = "https://hm.baidu.com/hm.js?584e04fe11f2faf26ddd5458633f86b4";
-var s = document.getElementsByTagName("script")[0]; 
-s.parentNode.insertBefore(hm, s);
-})();`}
-      </script>
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
